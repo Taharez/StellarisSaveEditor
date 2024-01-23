7e681f7e004704db196c683f9e46757b2d53f92e
Lars Rinde <lars@rinde.net>
2024-01-23 09:29:26 +0100

    Refactor BlazorRenderer to simplify

--- StellarisSaveEditor.BlazorWasm/Helpers/BlazorRenderer.cs
+++ StellarisSaveEditor.BlazorWasm/Helpers/BlazorRenderer.cs
@@ -3,44 +3,54 @@ using StellarisSaveEditor.Models;
 
 namespace StellarisSaveEditor.BlazorWasm.Helpers
 {
-    public static class BlazorRenderer
+    public class BlazorRenderer
     {
-        public static async Task Render(Canvas2DContext context, MapSettings mapSettings, GameState gameState, IEnumerable<string> markedFlags, string? searchSystemName)
-        {
-            await context.SetFillStyleAsync("black");
-            await context.FillRectAsync(0, 0, mapSettings.MapWidth, mapSettings.MapHeight);
+        private readonly Canvas2DContext _context;
+        private readonly MapSettings _mapSettings;
+        private readonly GameState _gameState;
 
-            await RenderSystems(context, gameState, mapSettings);
-            await RenderHyperLanes(context, gameState, mapSettings);
-            await RenderWormholes(context, gameState, mapSettings);
-            await RenderGateways(context, gameState, mapSettings);
-            await RenderLgates(context, gameState, mapSettings);
-            await RenderPlayerSystem(context, gameState, mapSettings);
-            await RenderMarkedSystemCoordinates(context, gameState, mapSettings, markedFlags);
-            await RenderMatchingNameSystemCoordinates(context, gameState, mapSettings, searchSystemName);
+        public BlazorRenderer(Canvas2DContext context, MapSettings mapSettings, GameState gameState) {
+            _context = context;
+            _mapSettings = mapSettings;
+            _gameState = gameState;
         }
 
-        private static async Task RenderSystems(Canvas2DContext context, GameState gameState, MapSettings mapSettings, int objectWidth = 2)
+        public async Task Render(IEnumerable<string> markedFlags, string? searchSystemName)
         {
-            await context.BeginBatchAsync();
-            await context.SetFillStyleAsync("white");
-            foreach (var galacticObject in gameState.GalacticObjects.Values)
+            await _context.SetFillStyleAsync("black");
+            await _context.FillRectAsync(0, 0, _mapSettings.MapWidth, _mapSettings.MapHeight);
+
+            await RenderSystems();
+            await RenderHyperLanes();
+            await RenderWormholes();
+            await RenderGateways();
+            await RenderLgates();
+            await RenderPlayerSystem();
+            await RenderMarkedSystemCoordinates(markedFlags);
+            await RenderMatchingNameSystemCoordinates(searchSystemName);
+        }
+
+        private async Task RenderSystems(int objectWidth = 2)
+        {
+            await _context.BeginBatchAsync();
+            await _context.SetFillStyleAsync("white");
+            foreach (var galacticObject in _gameState.GalacticObjects.Values)
             {
-                var c = mapSettings.GetModifiedCoordinate(galacticObject.Coordinate);
-                await context.FillRectAsync(c.X, c.Y, objectWidth, objectWidth);
+                var c = _mapSettings.GetModifiedCoordinate(galacticObject.Coordinate);
+                await _context.FillRectAsync(c.X, c.Y, objectWidth, objectWidth);
             }
-            await context.EndBatchAsync();
+            await _context.EndBatchAsync();
         }
 
-        private static async Task RenderHyperLanes(Canvas2DContext context, GameState gameState, MapSettings mapSettings)
+        private async Task RenderHyperLanes()
         {
-            if (!mapSettings.ShowHyperLanes)
+            if (!_mapSettings.ShowHyperLanes)
                 return;
 
-            await context.BeginBatchAsync();
-            await context.BeginPathAsync();
-            await context.SetStrokeStyleAsync("gray");
-            foreach (var galacticObject in gameState.GalacticObjects.Values)
+            await _context.BeginBatchAsync();
+            await _context.BeginPathAsync();
+            await _context.SetStrokeStyleAsync("gray");
+            foreach (var galacticObject in _gameState.GalacticObjects.Values)
             {
                 foreach (var hyperLane in galacticObject.HyperLanes)
                 {
@@ -48,130 +58,130 @@ namespace StellarisSaveEditor.BlazorWasm.Helpers
                     if (galacticObject.Id >= hyperLane.ToGalacticObjectIndex)
                         continue;
 
-                    var target = gameState.GalacticObjects[hyperLane.ToGalacticObjectIndex];
-                    var p1 = mapSettings.GetModifiedCoordinate(galacticObject.Coordinate);
-                    var p2 = mapSettings.GetModifiedCoordinate(target.Coordinate);
-                    await context.MoveToAsync(p1.X, p1.Y);
-                    await context.LineToAsync(p2.X, p2.Y);
+                    var target = _gameState.GalacticObjects[hyperLane.ToGalacticObjectIndex];
+                    var p1 = _mapSettings.GetModifiedCoordinate(galacticObject.Coordinate);
+                    var p2 = _mapSettings.GetModifiedCoordinate(target.Coordinate);
+                    await _context.MoveToAsync(p1.X, p1.Y);
+                    await _context.LineToAsync(p2.X, p2.Y);
                 }
             }
-            await context.StrokeAsync();
-            await context.EndBatchAsync();
+            await _context.StrokeAsync();
+            await _context.EndBatchAsync();
         }
 
-        private static async Task RenderBypasses(Canvas2DContext context, GameState gameState, MapSettings mapSettings, bool renderConnections, string bypassType, string color, double objectRadius = 5.0)
+        private async Task RenderBypasses(bool renderConnections, string bypassType, string color, double objectRadius = 5.0)
         {
-            await context.BeginBatchAsync();
-            await context.BeginPathAsync();
-            await context.SetStrokeStyleAsync(color);
-            foreach (var bypass in gameState.Bypasses.Values.Where(bp => bp.BypassType == bypassType))
+            await _context.BeginBatchAsync();
+            await _context.BeginPathAsync();
+            await _context.SetStrokeStyleAsync(color);
+            foreach (var bypass in _gameState.Bypasses.Values.Where(bp => bp.BypassType == bypassType))
             {
-                var hasObject = gameState.Indices.GalacticObjectByBypassId.TryGetValue(bypass.Id, out var galacticObject);
+                var hasObject = _gameState.Indices.GalacticObjectByBypassId.TryGetValue(bypass.Id, out var galacticObject);
                 if (!hasObject)
                     continue;
 
-                var p1 = mapSettings.GetModifiedCoordinate(galacticObject!.Coordinate);
-                await context.MoveToAsync(p1.X, p1.Y);
-                await context.ArcAsync(p1.X, p1.Y, objectRadius, 0, 360);
+                var p1 = _mapSettings.GetModifiedCoordinate(galacticObject!.Coordinate);
+                await _context.MoveToAsync(p1.X, p1.Y);
+                await _context.ArcAsync(p1.X, p1.Y, objectRadius, 0, 360);
 
                 if (renderConnections)
                 {
                     foreach (var connectedBypassId in bypass.Connections)
                     {
-                        var hasConnectedObject = gameState.Indices.GalacticObjectByBypassId.TryGetValue(connectedBypassId, out var connectedGalacticObject);
+                        var hasConnectedObject = _gameState.Indices.GalacticObjectByBypassId.TryGetValue(connectedBypassId, out var connectedGalacticObject);
                         if (!hasConnectedObject || connectedBypassId > bypass.Id)
                             continue;
-                        var p2 = mapSettings.GetModifiedCoordinate(connectedGalacticObject!.Coordinate);
-                        await context.MoveToAsync(p1.X, p1.Y);
-                        await context.LineToAsync(p2.X, p2.Y);
+                        var p2 = _mapSettings.GetModifiedCoordinate(connectedGalacticObject!.Coordinate);
+                        await _context.MoveToAsync(p1.X, p1.Y);
+                        await _context.LineToAsync(p2.X, p2.Y);
                     }
                 }
             }
-            await context.StrokeAsync();
-            await context.EndBatchAsync();
+            await _context.StrokeAsync();
+            await _context.EndBatchAsync();
         }
 
-        private static async Task RenderWormholes(Canvas2DContext context, GameState gameState, MapSettings mapSettings)
+        private async Task RenderWormholes()
         {
-            if (!mapSettings.ShowWormholes)
+            if (!_mapSettings.ShowWormholes)
                 return;
 
-            await RenderBypasses(context, gameState, mapSettings, true, "wormhole", "yellow");
+            await RenderBypasses(true, "wormhole", "yellow");
         }
 
-        private static async Task RenderGateways(Canvas2DContext context, GameState gameState, MapSettings mapSettings)
+        private async Task RenderGateways()
         {
-            if (!mapSettings.ShowGateways)
+            if (!_mapSettings.ShowGateways)
                 return;
 
-            await RenderBypasses(context, gameState, mapSettings, false, "gateway", "lightblue");
+            await RenderBypasses(false, "gateway", "lightblue");
         }
 
-        private static async Task RenderLgates(Canvas2DContext context, GameState gameState, MapSettings mapSettings)
+        private async Task RenderLgates()
         {
-            if (!mapSettings.ShowLgates)
+            if (!_mapSettings.ShowLgates)
                 return;
 
-            await RenderBypasses(context, gameState, mapSettings, false, "lgate", "purple");
+            await RenderBypasses(false, "lgate", "purple");
         }
 
-        private static async Task RenderPlayerSystem(Canvas2DContext context, GameState gameState, MapSettings mapSettings, double objectRadius = 5.0)
+        private async Task RenderPlayerSystem(double objectRadius = 5.0)
         {
-            if (!mapSettings.ShowHomeSystem)
+            if (!_mapSettings.ShowHomeSystem)
                 return;
 
-            var playerSystem = gameState.GalacticObjects[gameState.Countries[gameState.Player.CountryIndex].StartingSystemIndex];
-            var c = mapSettings.GetModifiedCoordinate(playerSystem.Coordinate);
-            await context.BeginBatchAsync();
-            await context.BeginPathAsync();
-            await context.SetStrokeStyleAsync("white");
-            await context.SetLineWidthAsync(2);
-            await context.MoveToAsync(c.X, c.Y);
-            await context.ArcAsync(c.X, c.Y, objectRadius, 0, 360);
-            await context.StrokeAsync();
-            await context.EndBatchAsync();
+            var playerSystem = _gameState.GalacticObjects[_gameState.Countries[_gameState.Player.CountryIndex].StartingSystemIndex];
+            var c = _mapSettings.GetModifiedCoordinate(playerSystem.Coordinate);
+            await _context.BeginBatchAsync();
+            await _context.BeginPathAsync();
+            await _context.SetStrokeStyleAsync("white");
+            await _context.SetLineWidthAsync(2);
+            await _context.MoveToAsync(c.X, c.Y);
+            await _context.ArcAsync(c.X, c.Y, objectRadius, 0, 360);
+            await _context.StrokeAsync();
+            await _context.EndBatchAsync();
         }
 
-        private static async Task RenderMarkedSystemCoordinates(Canvas2DContext context, GameState gameState, MapSettings mapSettings, IEnumerable<string> markedFlags, double objectRadius = 5.0)
+        private async Task RenderMarkedSystemCoordinates(IEnumerable<string> markedFlags, double objectRadius = 5.0)
         {
             if (markedFlags == null || !markedFlags.Any())
                 return;
 
             var markedSystemCoordinates = new List<Point>();
-            var markedSystems = gameState.GalacticObjects.Values.Where(o => o.GalacticObjectFlags.Any(markedFlags.Contains));
-            await context.BeginBatchAsync();
-            await context.BeginPathAsync();
-            await context.SetStrokeStyleAsync("lightgray");
-            await context.SetLineWidthAsync(2);
+            var markedSystems = _gameState.GalacticObjects.Values.Where(o => o.GalacticObjectFlags.Any(markedFlags.Contains));
+            await _context.BeginBatchAsync();
+            await _context.BeginPathAsync();
+            await _context.SetStrokeStyleAsync("lightgray");
+            await _context.SetLineWidthAsync(2);
             foreach (var markedSystem in markedSystems)
             {
-                var c = mapSettings.GetModifiedCoordinate(markedSystem.Coordinate);
-                await context.MoveToAsync(c.X, c.Y);
-                await context.ArcAsync(c.X, c.Y, objectRadius, 0, 360);
+                var c = _mapSettings.GetModifiedCoordinate(markedSystem.Coordinate);
+                await _context.MoveToAsync(c.X, c.Y);
+                await _context.ArcAsync(c.X, c.Y, objectRadius, 0, 360);
             }
-            await context.StrokeAsync();
-            await context.EndBatchAsync();
+            await _context.StrokeAsync();
+            await _context.EndBatchAsync();
         }
 
-        private static async Task RenderMatchingNameSystemCoordinates(Canvas2DContext context, GameState gameState, MapSettings mapSettings, string? searchSystemName, double objectRadius = 5.0)
+        private async Task RenderMatchingNameSystemCoordinates(string? searchSystemName, double objectRadius = 5.0)
         {
             if (searchSystemName == null)
                 return;
 
             var matchingNameSystemCoordinates = new List<Point>();
-            var matchingNameSystems = gameState.GalacticObjects.Values.Where(o => o.Name.Key.ToLower().StartsWith(searchSystemName));
-            await context.BeginBatchAsync();
-            await context.BeginPathAsync();
-            await context.SetStrokeStyleAsync("lightgray");
-            await context.SetLineWidthAsync(2);
+            var matchingNameSystems = _gameState.GalacticObjects.Values.Where(o => o.Name.Key.ToLower().StartsWith(searchSystemName));
+            await _context.BeginBatchAsync();
+            await _context.BeginPathAsync();
+            await _context.SetStrokeStyleAsync("lightgray");
+            await _context.SetLineWidthAsync(2);
             foreach (var matchingNameSystem in matchingNameSystems)
             {
-                var c = mapSettings.GetModifiedCoordinate(matchingNameSystem.Coordinate);
-                await context.MoveToAsync(c.X, c.Y);
-                await context.ArcAsync(c.X, c.Y, objectRadius, 0, 360);
+                var c = _mapSettings.GetModifiedCoordinate(matchingNameSystem.Coordinate);
+                await _context.MoveToAsync(c.X, c.Y);
+                await _context.ArcAsync(c.X, c.Y, objectRadius, 0, 360);
             }
-            await context.StrokeAsync();
-            await context.EndBatchAsync();
+            await _context.StrokeAsync();
+            await _context.EndBatchAsync();
         }
     }
 }
